f841b0fac6a3c62c459cde4ee8c2393750a0d072
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2022-06-21 23:52:02 +0200

    exercicios 4.1

---
+++ fundamentos/bloco-4-introducao-a-java-script/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,53 @@
+const a = 2;
+const b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+const c = 5;
+const d = 8;
+
+if (c > d) {
+  console.log(c);
+} else {
+  console.log(d);
+}
+
+const e = 3;
+const f = 4;
+const g = 9;
+
+if (e > f && e > g) {
+    console.log(e);
+} else if (f > e && f > g) {
+    console.log(f);
+} else {
+    console.log(g);
+}
+
+const h = -3;
+
+if (h > 0) {
+    console.log("positive");
+} else if (h < 0) {
+    console.log("negative");
+} else {
+    console.log(0);
+}
+
+const i = 30;
+const j = 30;
+const k = 190;
+var somaDosAngulos = i + j + k;
+
+if (somaDosAngulos === 180) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+const l = "cavalo";
+
